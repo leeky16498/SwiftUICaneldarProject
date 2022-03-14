2fa85b907902ce846a937e58920cde8471b1f04c
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-14 12:31:42 +0000

    Initial Commit

---
+++ SwiftUICaneldarProject/SwiftUICaneldarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICaneldarProjectApp.swift
+//  SwiftUICaneldarProject
+//
+//  Created by Kyungyun Lee on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICaneldarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
