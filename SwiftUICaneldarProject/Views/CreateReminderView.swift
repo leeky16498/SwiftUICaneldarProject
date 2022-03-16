d264aef4a5aac1a4488c1964e397d800768c1b19
chuljin Hwang <chljin.hwang@gmail.com>
2022-03-16 18:20:55 +0900

    clock

--- SwiftUICaneldarProject/Views/CreateReminderView.swift
+++ SwiftUICaneldarProject/Views/CreateReminderView.swift
@@ -10,14 +10,63 @@ import SwiftUI
 struct CreateReminderView: View {
     
     @State private var createReminderText:String = ""
-    
+
     var body: some View {
         VStack{
             inputTextTitle
             inputTextSection
             Divider()
-            reminderDetailsTitle
+//            reminderDetailsTitle
+            ZStack{
+                Circle()
+                    .frame(width: uiScreen().width * 0.9, height: uiScreen().width * 0.9, alignment: .center)
+                    .shadow(color: Color.caltheme.pink, radius: 20, x: 0, y: 0)
+                Circle()
+                    .frame(width: uiScreen().width * 0.7, height: uiScreen().width * 0.7, alignment: .center)
+                    .foregroundColor(Color.caltheme.black)
+                ForEach(1..<361, id: \.self){ sec in
+                    Rectangle()
+                            .frame(width: 20, height: 1)
+                            .foregroundColor(Color.pink)
+                            .offset(x: (uiScreen().width * 0.8 ) / 2)
+                            .rotationEffect(Angle(degrees: Double(sec)))
+                }
+                Rectangle()
+                        .frame(width: 120, height: 3)
+                        .foregroundColor(Color.caltheme.red)
+                        .offset(x: 60)
+                        .rotationEffect(Angle(degrees: Double(45)))
+                        
+                Circle()
+                    .frame(width: 15, height: 15, alignment: .center)
+                    .foregroundColor(Color.caltheme.red)
+            }
+         
+
+            Text("00:00")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.caltheme.secondaryText)
+                .padding()
+            
+            Button(action: {
+                
+            }, label: {
+                Text("Create Reminder")
+            })
+            .foregroundColor(Color.caltheme.secondaryText)
+            .font(.headline)
+            .frame(maxWidth: .infinity)
+            .frame(height:50)
+            .background(Color.caltheme.red)
+            .cornerRadius(10)
+            .padding()
+            
+            
         }
+    }
+    func uiScreen() -> CGRect{
+        return UIScreen.main.bounds
     }
 }
 
@@ -64,7 +113,6 @@ extension CreateReminderView {
                 )
         }
         .font(.title3)
-        .padding(.vertical,20)
         .background(
             RoundedRectangle(cornerRadius: 15)
                 .fill(Color.caltheme.background)
@@ -75,11 +123,11 @@ extension CreateReminderView {
     }
     
     private var reminderDetailsTitle :some View{
-        Text("Reminder Details")
+        Text("Reminder Timer")
             .font(.headline)
             .bold()
             .foregroundColor(Color.caltheme.secondaryText)
-            .frame(maxWidth:.infinity, alignment: .leading)
+            .frame(maxWidth:.infinity, alignment: .center)
             .padding(.vertical)
     }
 }
