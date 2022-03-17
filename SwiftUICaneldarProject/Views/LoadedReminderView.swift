169af8488b4ebe9de72a6c526f7efad344434498
chuljin Hwang <chljin.hwang@gmail.com>
2022-03-17 23:00:36 +0900

    LoadedView

---
+++ SwiftUICaneldarProject/Views/LoadedReminderView.swift
@@ -0,0 +1,27 @@
+//
+//  LoadedReminderView.swift
+//  SwiftUICaneldarProject
+//
+//  Created by Chuljin Hwang on 2022/03/17.
+//
+
+import SwiftUI
+import CoreData
+struct LoadedReminderView: View {
+    @StateObject var vm = ReminderViewModel()
+    var body: some View {
+        List{
+            ForEach(vm.savedEntity){ entity in
+                Text(entity.content ?? "no")
+                Text(entity.selectedColor ?? "no")
+                
+            }
+        }
+    }
+}
+
+struct LoadedReminderView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadedReminderView()
+    }
+}
