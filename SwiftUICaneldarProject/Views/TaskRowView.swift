d04d07c26e79a38125b479e8e74bf18a5a5e0648
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-19 10:05:22 +0000

    Task, Date 연결_중간

---
+++ SwiftUICaneldarProject/Views/TaskRowView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskRowView.swift
+//  SwiftUICaneldarProject
+//
+//  Created by Kyungyun Lee on 19/03/2022.
+//
+
+import SwiftUI
+
+struct TaskRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskRowView()
+    }
+}
